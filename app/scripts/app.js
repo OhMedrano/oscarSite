a0b87782e25cd3a98f71f9d0e7d9ee9fefbf7d6b
OhMedrano <Omedrano564@gmail.com>
2015-12-29 04:54:09 -0500

    New changes

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name oscarSiteApp
+ * @description
+ * # oscarSiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('oscarSiteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oscarSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the oscarSiteApp
+ */
+angular.module('oscarSiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
